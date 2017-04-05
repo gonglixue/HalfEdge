8175f846bfed099473ff674ea8c4106a6de07671
glx <glx@treehole.com>
2017-04-06 01:55:58 +0800

    read file to construct

--- HalfEdge/mesh.cpp
+++ HalfEdge/mesh.cpp
@@ -2,5 +2,85 @@
 
 void Mesh::ReadObj(string filename)
 {
+	ifstream input_file(filename);  // 打开obj文件
+	const int LINE_LENGTH = 100;
+	n_vertices = 0;
+	n_edges = 0;
+	n_faces = 0;
 
+	while (input_file) {
+		string type;
+		input_file >> type;  // 读取一词(space split) v or f
+		
+		if (type.length == 1) {
+			switch (type[0]) {
+			case '#': {
+				char comment[LINE_LENGTH];
+				input_file.getline(comment, LINE_LENGTH);
+				printf("# %s\n", comment);
+				break;
+			}
+			case 'v': {
+				float x, y, z;
+				input_file >> x >> y >> z;
+				Vertex vert(x, y, z, n_vertices);
+				this->vertices.push_back(vert);
+
+				n_vertices++;
+				break;
+			}
+			case 'f': {
+				int a, b, c; //面的顶点索引
+				input_file >> a >> b >> c;
+				// 创建3个HalfEdge
+				HalfEdge *he1, *he2, *he3;
+				he1->vert = &this->vertices[a];
+				he2->vert = &this->vertices[b];
+				he3->vert = &this->vertices[c];
+				// circular
+				he1->next = he2;
+				he2->next = he3;
+				he3->next = he1;
+				he1->prev = he3;
+				he2->prev = he1;
+				he3->prev = he2;
+				// 插入到map
+				EdgeIndexPair edgeIndex(a, b);
+				heMap.insert(map<EdgeIndexPair, HalfEdge*>::value_type(edgeIndex, he1));
+				edgeIndex = EdgeIndexPair(b, c);
+				heMap.insert(map<EdgeIndexPair, HalfEdge*>::value_type(edgeIndex, he2));
+				edgeIndex = EdgeIndexPair(c, a);
+				heMap.insert(map<EdgeIndexPair, HalfEdge*>::value_type(edgeIndex, he3));
+
+				// TODO Edge
+				// ...
+
+				// find pair
+				HalfEdgeMap::iterator mapIt = heMap.find(EdgeIndexPair(b,a));
+				if (mapIt != heMap.end()) {
+					// ab 存在pair ba
+					he1->pair = mapIt->second;  // 迭代器seconde就是value
+					
+				}
+
+				mapIt = heMap.find(EdgeIndexPair(c, b));
+				if (mapIt != heMap.end()) {
+					he2->pair = mapIt->second;
+				}
+				
+				mapIt = heMap.find(EdgeIndexPair(a, c));
+				if (mapIt != heMap.end()) {
+					he3->pair = mapIt->second;
+				}
+
+				// 此时对于三条边，halfEdge结构构建完毕
+				// 构建面
+				Face f(he1, he2, he3, n_faces);
+				this->faces.push_back(f);
+
+				n_faces++;
+			}
+			}
+		}
+	}
 }
--- HalfEdge/mesh.h
+++ HalfEdge/mesh.h
@@ -2,12 +2,19 @@
 #include <vector>
 #include <list>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
 using namespace std;
 
 struct HalfEdge;
 struct Vertex;
 struct Face;
 struct Edge;
+
+typedef pair<int, int> EdgeIndexPair;
+typedef map<EdgeIndexPair, HalfEdge*> HalfEdgeMap;
 
 struct HalfEdge {
 	Vertex* vert;
@@ -28,19 +35,42 @@ struct Vertex {
 	float color[3];
 	int id;
 
-	HalfEdge* halfEdge;  // outgoning edge
+	HalfEdge* halfEdge;  // outgoning edge？一个顶点的outgoing edge不是有很多吗？
+
+	Vertex(float x, float y, float z, int _id) {
+		position[0] = x;
+		position[1] = y;
+		position[2] = z;
+		id = _id;
+	}
 };
 
 struct Face {
 	HalfEdge* circularEdge[3];  // circularEdge[0]->next = circularEdge[1];
 	int id;
+
+	Face(Vertex* v0, Vertex* v1, Vertex* v2, int _id)
+	{
+		circularEdge[0]->vert = v0;
+		circularEdge[1]->vert = v1;
+		circularEdge[2]->vert = v2;
+		id = _id;
+	}
+	Face(HalfEdge* he1, HalfEdge* he2, HalfEdge* he3, int _id)
+	{
+		circularEdge[0] = he1;
+		circularEdge[1] = he2;
+		circularEdge[2] = he3;
+		id = _id;
+	}
 };
 
 class Mesh {
 public:
-	list<Vertex> vertices;
-	list<Face> faces;
-	list<Edge> edges;
+	vector<Vertex> vertices;
+	vector<Face> faces;
+	vector<Edge> edges;
+	HalfEdgeMap heMap;
 	int n_vertices, n_faces, n_edges;
 
 	void ReadObj(string filename);
